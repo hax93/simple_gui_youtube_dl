14e7b2c181098a85f49e812e648066a5ceaae1f0
hax93 <haxv1ct0ri0us@gmail.com>
2022-05-08 21:26:47 +0200

    option menu 'select quality'

--- gui_dl.py
+++ gui_dl.py
@@ -1,29 +1,47 @@
 import getpass
-import subprocess
 import os
+import subprocess
 import sys
-import time
-import youtube_dl
 import threading
+import time
 import tkinter as tk
-from tkinter import *
-from tkinter import ttk, messagebox
 from datetime import timedelta
+from tkinter import *
+from tkinter import messagebox, ttk
+
+import youtube_dl
+
 
 def download_function(url1, format):
+    #   download
+    formats_vid_aud = {
+        '1080':'bestvideo+bestaudio/best/1080',
+        '720':'mp4/720',
+        'mp3': 'bestaudio/mp3'
+    }
+    
     user = (getpass.getuser())
     video_url = url1
-    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
-    filename = f"C:\\Users\\{user}\\Downloads\\{video_info['title']}.mp4"
+    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url, 
+                                                     download=False)
+    filename = f"C:\\Users\\{user}\\Downloads\\{video_info['title']}"
 
     options={
         'progress_hooks': [callable_hook],
-        'format':format,
+        'format':formats_vid_aud.get(format),
         'keepvideo':False,
         'outtmpl':filename,
-        'source_address': '0.0.0.0' 
+        'source_address': '0.0.0.0', 
+        'quiet': True,
     }
 
+    if formats_vid_aud.get(format) == 'bestaudio/mp3':
+        options['postprocessors'] = [{'key': 'FFmpegExtractAudio',
+
+                'preferredcodec': 'm4a'}]
+    else:
+        pass
+    
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
     time.sleep(2)
@@ -55,7 +73,8 @@ def callable_hook(response):
     # data download
     try:
         if response["status"] == "downloading":
-            downloaded_percent = int((response["downloaded_bytes"]*100)/response["total_bytes"])
+            downloaded_percent = int(
+                (response["downloaded_bytes"]*100)/response["total_bytes"])
             downloaded_time = (response['eta'])
             td = timedelta(seconds=downloaded_time)
             percent.append(downloaded_percent)
@@ -65,6 +84,9 @@ def callable_hook(response):
         "No data..."
 
 class YouTubeDownload(ttk.Frame):
+    formats_video = ['-', '1080', '720']
+    formats_audio = ['-', 'mp3']
+    
     def __init__(self, container):
         super().__init__(container)
         # field options
@@ -82,7 +104,7 @@ class YouTubeDownload(ttk.Frame):
         
         # result label percent and eta
         self.result_label = Label(self, font="ariel 8 bold")
-        self.result_label.grid(padx=0, pady=41, column=1, row=3, sticky=tk.S)
+        self.result_label.grid(padx=0, pady=35, column=1, row=3, sticky=tk.S)
             
         # INPUT URL
         self.url = tk.StringVar()
@@ -90,18 +112,20 @@ class YouTubeDownload(ttk.Frame):
         self.url_entry.grid(column=1, row=1, ipadx = 110, sticky=tk.SW, **options)
         self.url_entry.focus()
              
-        # CHECK BOX VIDEO OR AUDIO  $VIDEO yt still no working
-        self.check = tk.StringVar()
-        self.check_boxes = Checkbutton(self, text='Video', 
-                                       command=self.agreement_changed, 
-                                       variable=self.check, 
-                                       onvalue='bestvideo/bestaudio/best')
+        # Option Menu VIDEO OR AUDIO  $VIDEO yt still no working
+        self.check_vid = tk.StringVar()
+        self.variable_video_set()
+        self.check_boxes = OptionMenu(self, self.check_vid,
+                                       *YouTubeDownload.formats_video,
+                                       command=self.agreement_changed)
         self.check_boxes.grid(column=1, row=2, sticky=tk.E)
+        
         # Audio
-        self.check_boxes = Checkbutton(self, text='Audio', 
-                                       command=self.agreement_changed, 
-                                       variable=self.check, 
-                                       onvalue='bestaudio/best')
+        self.check_aud = tk.StringVar()
+        self.variable_audio_set()
+        self.check_boxes = OptionMenu(self, self.check_aud,
+                                       *YouTubeDownload.formats_audio,
+                                       command=self.agreement_changed)
         self.check_boxes.grid(column=1, row=2, sticky=tk.S)
         
         # button download url
@@ -113,7 +137,7 @@ class YouTubeDownload(ttk.Frame):
         self.button_submit.grid(column=1, row=2, sticky=tk.W)
        
         # button destroy program
-        self.button_start = Button(self, text="Cancel", font="ariel 15 bold",
+        self.button_start = Button(self, text="Exit", font="ariel 15 bold",
                                    bg="black", fg="white")
         self.button_start['command'] = self.cancel_download
         self.button_start.grid(column=0, row=4, sticky=tk.S)
@@ -121,21 +145,31 @@ class YouTubeDownload(ttk.Frame):
         # button path download
         self.button_path = Button(self, text='File Download')
         self.button_path['command'] = self.open_path
-        self.button_path.grid(column=0, row=3, sticky=tk.S)
+        self.button_path.grid(column=0, row=3, ipady=10, sticky=tk.S)
         
         # add padding to the frame and show it
         self.grid(padx=10, pady=10, sticky=tk.NSEW)
+    
+    def variable_video_set(self):
+        self.check_vid.set(YouTubeDownload.formats_video[0]) 
+
+    def variable_audio_set(self):
+        self.check_aud.set(YouTubeDownload.formats_audio[0])
         
+    def agreement_changed(self):
+        # format for downloader_function
+        if self.check_vid.get() == '-' and self.check_aud.get() == 'mp3':
+            return self.check_aud.get()
+        if (self.check_aud.get() == '-' and 
+            self.check_vid.get() == '1080' or self.check_vid.get() == '720'):
+            return self.check_vid.get()
+
     def result_percent_eta(self):
         # return time ETA
         timer = f'{time_down[-1]}s'
         self.result_label.config(text=timer)
         self.after(3000, self.result_percent_eta)
              
-    def agreement_changed(self):
-        # format for downloader_function
-        return self.check.get()
-   
     def threading(self, work):
         # must for stability window
         t1=threading.Thread(target=work, daemon=True)
@@ -149,7 +183,7 @@ class YouTubeDownload(ttk.Frame):
         # download video/mp3
         download_function(self.url.get(), self.agreement_changed())
             
-    def cancel_download(self):  
+    def cancel_download(self):
         sys.exit()
         
     def update(self):
